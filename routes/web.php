62b54d894377231b405a97aa51cd41659810e412
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-21 21:10:42 +0500

    change the example file name to index

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 Route::get('/react', function () {
-    return view('welcome');
+    return view('index');
 });
