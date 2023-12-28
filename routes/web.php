11b26235210fcd2f9a775e5386375c18be5249c6
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-28 19:45:35 +0500

    all routes to index page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/react', function () {
-    return view('index');
-});
+Route::get('{any?}', function () {
+   return view('index');
+})->where('path', '.*');;
