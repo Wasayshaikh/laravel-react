89b2f15baf9b93404023e2d75be3cfa7d972a59f
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-28 19:45:23 +0500

    index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,19 @@
-@extends('layout.base')
-@section('content')
 
-<div class="py-3 ">
-    <h3>New Arrival</h3>
-    <div>
-        PRODUCTSs
-    </div>
-</div>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>React Laravel</title>
+    <link rel="stylesheet" href="{{url('css/tailwind.css')}}">
+   
+</head>
+<body class="bg-[#faf7f7]">
+<div id="app"></div>
+<!-- <div class="wrapper sm:w-full md:w-[720px] lg:w-[960px] xl:w-[1140px] 2xl:w-[1320px]  x m-auto px-4 sm:px-0"> -->
+<!-- <div class="wrapper sm:w-full md:w-[720px] lg:w-[960px] xl:w-[1140px] 2xl:w-[1320px]  x m-auto px-4 sm:px-0">
+
+</div> -->
+</body>
+<script src="{{url('js/app.js')}}"></script>
+</html>
